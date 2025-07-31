75dfb0be3c8802ad16c25298e9d6db47291153ae
Rushil Gupta <rushilcodes@gmail.com>
2025-07-31 08:50:48 +0530

    adding vercel speed insights

--- src/root.tsx
+++ src/root.tsx
@@ -1,9 +1,11 @@
 import { component$, isDev } from "@builder.io/qwik";
 import { QwikCityProvider, RouterOutlet } from "@builder.io/qwik-city";
 import { RouterHead } from "./components/router-head/router-head";
-import { injectSpeedInsights } from "@vercel/speed-insights";
 
 import "./global.css";
+
+import { injectSpeedInsights } from "@vercel/speed-insights";
+  injectSpeedInsights();
 
 export default component$(() => {
   /**
@@ -13,7 +15,6 @@ export default component$(() => {
    * Don't remove the `<head>` and `<body>` elements.
    */
 
-  injectSpeedInsights();
 
   return (
     <QwikCityProvider>
